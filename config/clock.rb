c4e2e1ae97d03652c18a6ad2ba4b15b558bce588
Ali Fadel <aliosm1997@gmail.com>
2018-06-29 23:45:06 +0300

    Add CodeforcesGym update contests periodic task to clockwork

--- config/clock.rb
+++ config/clock.rb
@@ -6,8 +6,11 @@ module Clockwork
   handler do |job|
     if job.eql?('frequent.update_codeforces')
 			Codeforces.update_contests
+    elsif job.eql?('frequent.update_codeforces_gym')
+      CodeforcesGym.update_contests
 		end
   end
 
   every(30.minute, 'frequent.update_codeforces')
+  every(30.minute, 'frequent.update_codeforces_gym')
 end
