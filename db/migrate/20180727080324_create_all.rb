f68c7a328404d2b2856c1c309c97e35060a9d621
Ali Fadel <aliosm1997@gmail.com>
2018-07-27 11:57:18 +0300

    Remove id column from All model

--- db/migrate/20180727080324_create_all.rb
+++ db/migrate/20180727080324_create_all.rb
@@ -1,6 +1,6 @@
 class CreateAll < ActiveRecord::Migration[5.2]
   def change
-    create_table :all do |t|
+    create_table :all, id: false do |t|
       t.string :name
       t.string :start_time
       t.string :duration
