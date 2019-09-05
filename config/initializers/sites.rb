6753b27101856a0f16fb2ce87f53e37a14e14873
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2019-09-05 22:27:07 +0300

    Allow CS Academy and HackerEarth to update

--- config/initializers/sites.rb
+++ config/initializers/sites.rb
@@ -2,11 +2,11 @@ SITES = [
   ['All', 'all', 'https://www.kontests.net'],
   ['CodeForces', 'codeforces', 'https://www.codeforces.com'],
   ['CodeForces::Gym', 'codeforces_gym', 'https://www.codeforces.com/gyms'],
-  # ['CS Academy', 'cs_academy', 'https://csacademy.com'],
+  ['CS Academy', 'cs_academy', 'https://csacademy.com'],
   ['AtCoder', 'at_coder', 'https://atcoder.jp'],
   # ['HackerRank', 'hacker_rank', 'https://www.hackerrank.com'],
   ['CodeChef', 'code_chef', 'https://www.codechef.com'],
-  # ['HackerEarth', 'hacker_earth', 'https://www.hackerearth.com'],
+  ['HackerEarth', 'hacker_earth', 'https://www.hackerearth.com'],
   ['A2OJ', 'a2oj', 'https://www.a2oj.com']
 ].freeze
 
