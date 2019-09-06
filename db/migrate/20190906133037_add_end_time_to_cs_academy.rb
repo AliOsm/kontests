0f7057ddcf1d5640b45b6f47eb1a246d299ab273
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2019-09-06 16:38:57 +0300

    Fix migration typo

--- db/migrate/20190906133037_add_end_time_to_cs_academy.rb
+++ db/migrate/20190906133037_add_end_time_to_cs_academy.rb
@@ -1,16 +1,16 @@
 class AddEndTimeToCsAcademy < ActiveRecord::Migration[5.2]
   def change
   	# We need to add end_time after start_time, so remove any column after start_time
-  	remove_column :at_coder, :duration
-  	remove_column :at_coder, :in_24_hours
-  	remove_column :at_coder, :status
+  	remove_column :cs_academy, :duration
+  	remove_column :cs_academy, :in_24_hours
+  	remove_column :cs_academy, :status
 
   	# Add end_time column
-  	add_column :at_coder, :end_time, :string
+  	add_column :cs_academy, :end_time, :string
 
   	# Add the previously removed columns
-  	add_column :at_coder, :duration, :string
-  	add_column :at_coder, :in_24_hours, :string
-  	add_column :at_coder, :status, :string
+  	add_column :cs_academy, :duration, :string
+  	add_column :cs_academy, :in_24_hours, :string
+  	add_column :cs_academy, :status, :string
   end
 end
