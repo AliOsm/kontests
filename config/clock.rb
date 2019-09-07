a5174c3dfe0e48d0efa69667a3d1e489d4e155b8
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2019-09-07 14:57:39 +0300

    Reduce the number of threads

--- config/clock.rb
+++ config/clock.rb
@@ -6,19 +6,25 @@ module Clockwork
   handler do |job|
     if job.eql?('frequent.ping')
       PingService.ping
+    elsif job.eql?('frequent.cf_cfg_ac')
+      UpdateSitesService.codeforces
+      UpdateSitesService.codeforces_gym
+      UpdateSitesService.at_coder
+    elsif job.eql?('frequent.cc_a2')
+      UpdateSitesService.code_chef
+      UpdateSitesService.a2oj
+    elsif job.eql?('frequent.lc_csa_he')
+      UpdateSitesService.leet_code
+      UpdateSitesService.cs_academy
+      UpdateSitesService.hacker_earth
     else
       UpdateSitesService.send(job.split('.')[-1])
     end
   end
 
   every(2.minute, 'frequent.all')
-  every(3.minute, 'frequent.codeforces')
-  every(3.minute, 'frequent.codeforces_gym')
-  every(3.minute, 'frequent.at_coder')
-  every(5.minute, 'frequent.code_chef')
-  every(5.minute, 'frequent.a2oj')
-  every(7.minute, 'frequent.leet_code')
-  every(7.minute, 'frequent.cs_academy')
-  every(11.minute, 'frequent.hacker_earth')
+  every(3.minute, 'frequent.cf_cfg_ac') # codeforces, codeforces_gym, at_coder
+  every(5.minute, 'frequent.cc_a2') # code_chef, a2oj
+  every(7.minute, 'frequent.lc_csa_he') # leet_code, cs_academy, hacker_earth
   every(29.minute, 'frequent.ping')
 end
