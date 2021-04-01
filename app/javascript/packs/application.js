33da772ad469c77632b80fd59fdb362b17af872c
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2021-04-02 01:13:56 +0300

    Add pack stypesheets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import '../stylesheets/application'
+
 require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
