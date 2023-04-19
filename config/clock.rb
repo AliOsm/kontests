491c46a0100bd8b5292baf7dc612e22b625af829
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-04-19 23:53:14 +0300

    Stop CodeChef and Toph for now, they are raising exceptions...

--- config/clock.rb
+++ config/clock.rb
@@ -15,14 +15,14 @@ module Clockwork
       TopCoderService.new.update_contests
       AtCoderService.new.update_contests
     when 'frequent.5_min'
-      CodeChefService.new.update_contests
+#       CodeChefService.new.update_contests
       HackerRankService.new.update_contests
       HackerEarthService.new.update_contests
       LeetCodeService.new.update_contests
     when 'frequent.7_min'
       CsAcademyService.new.update_contests
       KickStartService.new.update_contests
-      TophService.new.update_contests
+#       TophService.new.update_contests
     end
   end
 
